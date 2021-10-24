f448b989183bff3123d44f396353e377f5812f1b
hopkuba <hopkuba@gmail.com>
2021-10-24 07:59:07 +0200

    Final Release 0.2

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const infogetter = require('./infogetter.js')
 const path = require("path")
 
 app.whenReady().then(() => {
-  app.on('window-all-closed', e => e.preventDefault() )
-
-  const greendot = nativeImage.createFromPath(path.resolve("./trayicon_greendot.png"))
-  const reddot = nativeImage.createFromPath(path.resolve("./trayicon_reddot.png"))
-  appIcon = new Tray(nativeImage.createFromPath(path.resolve("./trayicon.png")))
+  app.on('window-all-closed', e => e.preventDefault())
+  var suffix = ".png"
+  if(process.platform === "win32"){
+    suffix=".ico"
+  }
+  const greendot = path.join(__dirname, 'trayicon_greendot'+suffix)
+  const reddot = path.join(__dirname, 'trayicon_reddot'+suffix)
+  appIcon = new Tray(path.join(__dirname, 'trayicon'+suffix))
+  
   appIcon.setToolTip("FGCompanion")
 
   const contextMenu = Menu.buildFromTemplate(
@@ -63,7 +67,7 @@ app.whenReady().then(() => {
           details: `Flying over ${info.airspace}`,
           startTimestamp: Date.now(),
           endTimestamp: Date.now() + 15000,
-          largeImageKey: info.pngn.toLowerCase(),
+          largeImageKey: info.icon.toLowerCase(),
           largeImageText: info.aircraft,
           smallImageKey: "paint",
           smallImageText: info.paintjobtext,
@@ -71,6 +75,7 @@ app.whenReady().then(() => {
         })
         appIcon.setImage(greendot)
       },(error) => {
+        console.log(error)
         appIcon.setImage(reddot)
       })
     setTimeout(updateRPC,15000)
@@ -79,6 +84,7 @@ app.whenReady().then(() => {
   try{
     client = require('discord-rich-presence')('900398628529664030')
   }catch(err){
+    console.log(err)
     appIcon.setImage(reddot)
   }
   contextMenu.items[0].checked = true
